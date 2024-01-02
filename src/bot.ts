f8f2b8a1f3ac57fa3451a1d54cc419a78917774e
Ezedin Nuru <121353252+ezedin85@users.noreply.github.com>
2024-01-02 18:18:45 +0300

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -8,7 +8,7 @@ import type { Variant as TextEffectVariant } from "./textEffects";
 // Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
-// Handle the /yo command to greet the user
+// Handle the /yo command to greet the user --test--
 bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
 
 // Handle the /effect command to apply text effects using an inline keyboard
